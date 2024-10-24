5b321182147dcf9037bf809d24dd5fe2dd904020
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-10-24 23:01:40 +0200

    refactor: change error name

--- shared/src/main/scala/it/unibo/flexmultimod/language/meta/LanguageMacro.scala
+++ shared/src/main/scala/it/unibo/flexmultimod/language/meta/LanguageMacro.scala
@@ -6,10 +6,10 @@ import it.unibo.flexmultimod.tier.Peer
 import scala.quoted.{Expr, Quotes, Type}
 
 object LanguageMacro:
-  inline def asLocalMacro[Value, LocalNode <: Peer, RemoteNode <: Peer](func: Value on RemoteNode)(using placed: Placed[LocalNode]) =
-    ${ asLocalImpl('func, 'placed) }
+  inline def remoteRefMacro[Value, LocalNode <: Peer, RemoteNode <: Peer](func: Value on RemoteNode)(using placed: Placed[LocalNode]) =
+    ${ remoteRefMacroImpl('func, 'placed) }
 
-  private def asLocalImpl[Value, LocalNode <: Peer: Type, RemoteNode <: Peer: Type](
+  private def remoteRefMacroImpl[Value, LocalNode <: Peer: Type, RemoteNode <: Peer: Type](
       expr: Expr[Value on RemoteNode],
       givenData: Expr[Placed[LocalNode]]
   )(using quote: Quotes): Expr[Value] =
@@ -18,8 +18,8 @@ object LanguageMacro:
     val localType = TypeRepr.of[LocalNode]
     if (remoteType =:= localType)
       report.error(s"""
-        The value is placed at peer ${remoteType.show} which is also the current placement (${localType.show}).
+        Attempt to retrieve a remote value but it is placed locally at: ${localType.show}
 
-        To retrieve a value placed on the local placement use the `bind` method instead.
+        To retrieve a local value use the `bind` method instead.
       """.strip())
     '{ ??? }
