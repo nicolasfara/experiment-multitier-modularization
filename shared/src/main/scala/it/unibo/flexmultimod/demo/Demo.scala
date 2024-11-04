99ac5229c05452372f237d9f5a7cf4eb38d08cc3
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-11-04 22:28:17 +0100

    Increase demo complexity

--- shared/src/main/scala/it/unibo/flexmultimod/demo/Demo.scala
+++ shared/src/main/scala/it/unibo/flexmultimod/demo/Demo.scala
@@ -3,37 +3,47 @@ package it.unibo.flexmultimod.demo
 import it.unibo.flexmultimod.language.FlexMultiModLanguage.*
 import it.unibo.flexmultimod.language.FlexMultiModLanguage.Language.*
 import it.unibo.flexmultimod.tier.{Multiple, Peer}
+import scala.compiletime.constValue
+import scala.compiletime.ops.int.*
 
 trait WithAi
-trait WithRam[RamGb <: Int]
-trait WithTempSensor
+trait WithGps
 
-trait Gradient[Node <: Peer & WithAi]:
+trait Gradient[Node <: Peer & WithGps]:
   def gradientCast[Value](source: Boolean, value: Value): Value on Node
 
 object Gradient:
-  def apply[Node <: Peer & WithAi](): Gradient[Node] = new Gradient[Node]():
+  def apply[Node <: Peer & WithGps](): Gradient[Node] = new Gradient[Node]():
     def gradientCast[Value](source: Boolean, value: Value): Value on Node = ???
 
-trait GreaterDistance[Node <: Peer & (WithTempSensor | WithRam[1])]:
+trait GreaterDistance[Node <: Peer]:
   def isGreaterThan[Value](value: Value, threshold: Value): Boolean on Node
 
 object GreaterDistance:
-  def apply[Node <: Peer & (WithTempSensor | WithRam[1])](): GreaterDistance[Node] = new GreaterDistance[Node]():
+  def apply[Node <: Peer](): GreaterDistance[Node] = new GreaterDistance[Node]():
     def isGreaterThan[Value](value: Value, threshold: Value): Boolean on Node = ???
 
-trait Infrastructure:
-  type Application <: Peer & WithRam[1] {type Tie <: Multiple[Infrastructural]}
-  type Infrastructural <: Peer & WithAi {type Tie <: Multiple[Application]}
+trait HeavyComputation[Node <: Peer & WithAi]:
+  def compute[Value](value: Value): Value on Node
+
+object HeavyComputation:
+  def apply[Node <: Peer & WithAi](): HeavyComputation[Node] = new HeavyComputation[Node]():
+    def compute[Value](value: Value): Value on Node = ???
 
 trait MyApplication extends Language:
-  type Application <: Peer & WithRam[1] { type Tie <: Multiple[Infrastructural] }
-  type Infrastructural <: Peer & WithAi { type Tie <: Multiple[Application] }
+  type Node <: Peer { type Tie <: Multiple[Node] }
+  type Application <: Node { type Tie <: Multiple[Infrastructural] }
+  type Infrastructural <: Node { type Tie <: Multiple[Application] }
+  type EdgeServer <: Infrastructural & WithAi
+  type CloudInstance <: Infrastructural
+  type Smartphone <: Application & WithGps
 
-  private val gradient = Gradient[Infrastructural]()
-  private val greaterDistance = GreaterDistance[Application]()
+  private val gradient = Gradient[Smartphone]()
+  private val greaterDistance = GreaterDistance[CloudInstance]()
+  private val heavyComputation = HeavyComputation[EdgeServer]()
 
-  def macroProgram(): Unit = program[Application]:
-    val value = gradient.gradientCast(true, 10.0).remoteRef
-//    greaterDistance.isGreaterThan(10.3, 5.0).remoteRef
-    val bar = greaterDistance.isGreaterThan(value, 5.0).bind
+  def macroProgram(): Unit = program[Smartphone]:
+    val value = gradient.gradientCast(true, 10.0).bind
+    val bar = greaterDistance.isGreaterThan(value, 5.0).remoteRef
+    val result = heavyComputation.compute(bar).remoteRef
+    println(result)
