a8457d4800ca22ee3b7bf8f87eb9ff3b94c24312
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-01-28 21:49:29 +0100

    Define a mini use case

--- src/main/scala/it/unibo/mode2/Demo.scala
+++ src/main/scala/it/unibo/mode2/Demo.scala
@@ -1,14 +1,38 @@
 package it.unibo.mode2
 
-import it.unibo.mode2.language.Component
+import it.unibo.mode2.language.{Application, Component, Infrastructural}
 
-trait Foo
-trait Bar
-trait Baz
+trait WithAccelerometer
+trait WithGps
+trait WithAlert
 
 object MovementDetection extends Component[EmptyTuple, Double]:
-  override type Capabilities >: Foo | Baz
+  override type Capabilities >: WithAccelerometer | WithGps
   override def apply(input: EmptyTuple): Double = ???
 
-trait DistanceBetween extends Component[EmptyTuple, Double]:
+object DistanceBetween extends Component[EmptyTuple, Double]:
+  override type Capabilities >: WithGps
   override def apply(input: EmptyTuple): Double = ???
+
+object EmergencyDetection extends Component[Double *: Double *: EmptyTuple, Boolean]:
+  override type Capabilities >: WithAlert
+  override def apply(input: Double *: Double *: EmptyTuple): Boolean = ???
+
+trait Smartphone[ID] extends Application[ID]:
+  override type Capabilities = WithAccelerometer & WithGps & WithAlert
+trait Wearable[ID] extends Infrastructural[ID]:
+  override type Capabilities = WithAccelerometer
+
+object ApplicationSmartphone extends Smartphone[1]
+object InfrastructuralWearable extends Wearable[1]
+
+def infrastructureSpecification(): Unit =
+  import it.unibo.mode2.language.InfrastructuralDsl.*
+  MovementDetection deployedOn InfrastructuralWearable forDevice ApplicationSmartphone
+  DistanceBetween deployedOn ApplicationSmartphone forDevice ApplicationSmartphone
+  EmergencyDetection deployedOn ApplicationSmartphone forDevice ApplicationSmartphone
+
+def macroProgram(): Unit =
+  val movementSpeed = MovementDetection(EmptyTuple)
+  val distance = DistanceBetween(EmptyTuple)
+  EmergencyDetection(movementSpeed *: distance *: EmptyTuple)
--- src/main/scala/it/unibo/mode2/language/InfrastructuralDevice.scala
+++ src/main/scala/it/unibo/mode2/language/InfrastructuralDevice.scala
@@ -1,35 +1,23 @@
 package it.unibo.mode2.language
 
-import it.unibo.mode2.{Bar, Foo, MovementDetection}
-
 sealed trait Device[ID: Ordering]:
   type Capabilities
 trait Application[ID] extends Device[ID]
 trait Infrastructural[ID] extends Device[ID]
 
-trait Smartphone[ID] extends Application[ID]:
-  override type Capabilities = Bar
-trait Wearable[ID] extends Infrastructural[ID]:
-  override type Capabilities = Foo
-
-object ApplicationSmartphone extends Smartphone[1]
-object Smartphone2 extends Smartphone[2]
-object InfrastructuralWearable extends Wearable[1]
-
 class Placement[C <: Component[?, ?], D <: Device[?]](val component: C, val device: D)(using
     device.Capabilities <:< component.Capabilities
 )
 
-class Pippo[A <: Application[?], C <: Component[?, ?], D <: Device[?]](
+class Deployment[A <: Application[?], C <: Component[?, ?], D <: Device[?]](
     val applicationDevice: A,
     val placement: Placement[C, D]
 )
 
-extension [C <: Component[?, ?], D <: Device[?]](component: C)
-  infix def deployedOn(device: D)(using device.Capabilities <:< component.Capabilities): Placement[C, D] =
-    Placement(component, device)
+object InfrastructuralDsl:
+  extension [C <: Component[?, ?], D <: Device[?]](component: C)
+    infix def deployedOn(device: D)(using device.Capabilities <:< component.Capabilities): Placement[C, D] =
+      Placement(component, device)
 
-extension [A <: Application[?], C <: Component[?, ?], D <: Device[?]](placement: Placement[C, D])
-  infix def forDevice(device: A): Pippo[A, C, D] = Pippo(device, placement)
-
-val deployment = MovementDetection deployedOn InfrastructuralWearable forDevice ApplicationSmartphone
+  extension [A <: Application[?], C <: Component[?, ?], D <: Device[?]](placement: Placement[C, D])
+    infix def forDevice(device: A): Deployment[A, C, D] = Deployment(device, placement)
