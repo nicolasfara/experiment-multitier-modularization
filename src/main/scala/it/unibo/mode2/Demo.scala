b08b100f030c88519e0fbe7e30bc2553473a4b7c
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-02-03 10:50:16 +0100

    improve case study

--- src/main/scala/it/unibo/mode2/Demo.scala
+++ src/main/scala/it/unibo/mode2/Demo.scala
@@ -22,21 +22,36 @@ object EmergencyDetection extends Component[Double *: Double *: EmptyTuple, Aggr
 
 trait Smartphone extends Application:
   override type Capabilities = WithAccelerometer & WithGps & WithAlert
+  override type Tie <: Smartphone & Wearable
 
 trait Wearable extends Infrastructural:
   override type Capabilities = WithAccelerometer
+  override type Tie <: Wearable & Smartphone
 
-object ApplicationSmartphone extends Smartphone
-object InfrastructuralWearable extends Wearable
+trait Edge extends Infrastructural:
+  override type Capabilities = Any
+  override type Tie <: Edge & Smartphone
+
+object Smartphone1 extends Smartphone
+object Smartphone2 extends Smartphone
+object Wearable1 extends Wearable
+object Wearable2 extends Wearable
+
+//object ApplicationSmartphone extends Smartphone
+//object InfrastructuralWearable extends Wearable
 
 import it.unibo.mode2.language.InfrastructuralDsl.*
 
 def infrastructureSpecification(): Any =
   deployment:
-    forDevice(ApplicationSmartphone):
-      MovementDetection deployedOn InfrastructuralWearable
-      DistanceBetween deployedOn ApplicationSmartphone
-      EmergencyDetection deployedOn ApplicationSmartphone
+    forDevice(Smartphone1):
+      MovementDetection deployedOn Wearable1
+      DistanceBetween deployedOn Smartphone1
+      EmergencyDetection deployedOn Smartphone1
+    forDevice(Smartphone2):
+      MovementDetection deployedOn Wearable2
+      DistanceBetween deployedOn Smartphone2
+      EmergencyDetection deployedOn Smartphone2
 
 def macroProgram(): Any =
   val movementSpeed = MovementDetection(EmptyTuple)
